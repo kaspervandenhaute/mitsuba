a6203a2eac2aff0f789e9eec4fad27e7783912d7
Kasper <vanderkasper@gmail.com>
2021-04-11 17:28:16 +0200

    random initialisation without performance hit

--- src/integrators/myIntegrator/myRplSampler.h
+++ src/integrators/myIntegrator/myRplSampler.h
@@ -12,8 +12,8 @@ class MyRplSampler : public ReplayableSampler {
 public:
     void reSeed(uint64_t seed) {
         m_initial = new Random(seed);
-        m_random = new Random();
-        m_random->set(m_initial);
+        m_random = new Random(m_initial);
+        // m_random->set(m_initial);
         m_sampleIndex = 0;
         // m_sampleCount = 0;
     }
